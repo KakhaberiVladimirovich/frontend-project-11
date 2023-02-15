3728b8a3249388ef163aecee687fead559161bcf
Бугианишвили <skulaxo98@mail.ru>
2023-02-15 18:24:20 +0600

    step1

--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,9 @@ const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
   mode: process.env.NODE_ENV || 'development',
+  watchOptions: {
+    poll: 1000,
+  },
   module: {
     rules: [
       {
