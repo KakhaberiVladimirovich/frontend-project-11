d7681bb96bca914639ab83fc0579a12c8733fe53
Бугианишвили <skulaxo98@mail.ru>
2023-02-16 19:43:57 +0600

    fix step1

--- src/init.js
+++ src/init.js
@@ -7,14 +7,4 @@ export default () => {
   const element = document.getElementById('point');
   const obj = new Example(element);
   obj.init();
-
-  const triggerTabList = document.querySelectorAll('#list-tab2 a');
-  triggerTabList.forEach((triggerEl) => {
-    const tabTrigger = new bootstrap.Tab(triggerEl);
-
-    triggerEl.addEventListener('click', (event) => {
-      event.preventDefault();
-      tabTrigger.show();
-    });
-  });
 };
